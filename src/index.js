914b8866a0994c7bf9e1e9d31599037c8bec7207
paulotijero <paulotijero@gmail.com>
2019-08-25 13:08:18 -0500

    Add font, logo and install @emotion/core, @reach/router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+/** @jsx jsx */
 import React from "react";
 import { render } from "react-dom";
+import { Router } from "@reach/router";
+import { Global, jsx } from "@emotion/core";
+
+import Home from "./views/home";
+
+const global = {
+  body: {
+    margin: 0,
+    fontFamily: "'Press Start 2P', cursive"
+  }
+};
 
 function App() {
-  return <h1>OpenTech</h1>;
+  return (
+    <>
+      <Global styles={global} />
+      <Router>
+        <Home path="/" />
+      </Router>
+    </>
+  );
 }
 
 render(<App />, document.getElementById("root"));
