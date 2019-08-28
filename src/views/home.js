2354ed0368d0482d5619e10954931080df643a3e
paulotijero <paulotijero@gmail.com>
2019-08-28 17:15:19 -0500

    Add venue

--- src/views/home.js
+++ src/views/home.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Slogan from "../components/slogan";
 import About from "../components/about";
+import Venue from "../components/venue";
 
 function Home() {
   return (
     <>
       <Slogan />
       <About />
+      <Venue />
     </>
   );
 }
