b737078737cdbb8643e784d34f4cf85233b6631c
paulotijero <paulotijero@gmail.com>
2019-09-01 20:37:37 -0500

    Add map

--- src/components/venue.js
+++ src/components/venue.js
@@ -18,9 +18,15 @@ function Venue() {
         <Center>
           <SubTitle>Ubicación</SubTitle>
           <span>
-            Facultad de Ingeniería en Informática y Sistemas,Tingo María, Peru
+            Facultad de Ingeniería en Informática y Sistemas, Tingo María, Peru
           </span>
-          <div css={mapa}></div>
+          <iframe
+            src="https://maps.google.com/maps?q=Facultad%20de%20Ingenier%C3%ADa%20en%20Inform%C3%A1tica%20y%20Sistemas%2C%20E%200%20Rd%2C%20Peru%20&t=&z=17&ie=UTF8&iwloc=&output=embed"
+            frameBorder="0"
+            allowFullScreen
+            title="Able, Oficina 902, Jorge Chávez 184, Miraflores 15074, Lima, Perú"
+            css={mapa}
+          />
         </Center>
       </section>
     </Section>
