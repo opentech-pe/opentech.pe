204af4eafcfd8222a9364452c964ac1b5d7a00dc
paulotijero <paulotijero@gmail.com>
2020-04-19 19:04:49 -0500

    ADD: config theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    fontFamily: {
+      big: ['"Press Start 2P"', "sans-serif"],
+      small: ["VT323", "sans-serif"],
+    },
     extend: {},
   },
   variants: {},
   plugins: [],
-}
+};
