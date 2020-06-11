4f2d60b44d592f80b41e5b42427afcfc76b639a3
paulotijero <paulotijero@gmail.com>
2020-06-11 13:23:40 -0500

    FEAT: navbar

--- components/navbar.js
+++ components/navbar.js
@@ -2,23 +2,23 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <header className="flex justify-between items-center">
+    <header className="flex justify-between items-center font-big py-8">
       <Link href="/">
-        <a title="OpenTech">
-          Open<span>Tech</span>
+        <a title="OpenTech" className="text-3xl">
+          Open<span className="text-blue-500">Tech</span>
         </a>
       </Link>
       <nav>
-        <ul>
-          <li>
+        <ul className="flex items-center">
+          <li className="w-48 text-center mr-6">
             <Link href="/previous-events">
               <a title="Eventos anteriores">Eventos anteriores</a>
             </Link>
           </li>
-          <li>
-            <a href="https://github.com/opentech-pe">
-              <img src="/assets/img/github.gif" alt="github" />
-              Github
+          <li className="flex justify-center w-48">
+            <a href="https://github.com/opentech-pe" className="flex items-center">
+              <img src="/assets/img/github.gif" alt="github" className="mr-2 h-8 w-8"/>
+              <span>Github</span>
             </a>
           </li>
         </ul>
