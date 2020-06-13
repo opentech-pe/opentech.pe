b3bb47d18ec0a847bd35f53639c081435d6a68ae
paulotijero <paulotijero@gmail.com>
2020-06-13 08:22:12 -0500

    FEAT: effect 404

--- pages/404.js
+++ pages/404.js
@@ -1,18 +1,25 @@
 import Link from "next/link";
 
 export default function Custom404(){
-    return (
+  const [visible, setVisible] = React.useState(true)
+  
+  function handleEvent(){
+    setVisible(!visible)
+  }
+
+  return (
       <section className="flex flex-col items-center justify-center h-screen px-3">
         <h1 className="flex text-9xl md:text-12xl">
           4
-          <img className="h-40 mr-4 md:h-48" src="/assets/img/death.png" />
+          <img className={`h-40 mr-4 md:h-48 ${visible ? '' : 'hidden'}`} src="/assets/img/death.png" alt="mac-dead"/>
+          <img className={`h-40 mr-4 md:h-48 ${!visible ? '' : 'hidden'}`} src="/assets/img/life.png" alt="mac-life"/>
           4
         </h1>
         <span className="font-big mt-8 text-center md:mt-2">This page could not be found.</span>
         <Link href="/">
-          <a className="font-small m-5 text-xl" title="Go Back Home">
+          <button onMouseOver={handleEvent} onMouseOut={handleEvent} className="font-small m-5 text-xl hover:cursor-pointer">
             {"<--"} Go Back Home
-          </a>
+          </button>
         </Link>
       </section>
     )
