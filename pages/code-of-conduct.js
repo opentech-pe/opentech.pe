f40c1a5cafb54572ad5eda57072f798f0b7a9b90
Jimmy Loloy <loloy.laurencio@gmail.com>
2020-06-15 01:13:59 -0500

    ADD: text full code of conduct

--- pages/code-of-conduct.js
+++ pages/code-of-conduct.js
@@ -12,22 +12,22 @@ export default function CodeOfConduct() {
     </Head>
     <Navbar />
     <main className="px-3 md:px-48">
-      <section className="mb-20 mt-10">
+      <section className="mb-16 mt-10">
         <h1 className="font-big text-center md:text-5xl">
           Código de conducta
         </h1>
       </section>
-      <section className="my-20">
-        <p className="font-small text-2xl">
+      <section className="my-16">
+        <p>
           Se requiere que todos los asistentes, ponentes, patrocinadores y voluntarios de
           nuestro meetup <em>(sea presencial u online),</em> acepten el siguiente codigo de conducta. 
           Los organizadores harán cumplir este código a lo largo del evento. Esperamos la 
           colaboración de todos los participantes para ayudar a asegurar un ambiente seguro para todos.
         </p>
       </section>
-      <section className="my-20">
+      <section className="my-16">
         <h2 className="font-big mb-5 text-xl">La Versión Rápida</h2>
-        <p className="font-small text-2xl">
+        <p>
           Nuestro meetup se dedica a ofrecer una experiencia libre de abusos para todos, 
           independientemente de su genero, orientación sexual, discapacidad, apariencia física, 
           talla, raza o religión. No toleramos abusos de los participantes a la conferencia 
@@ -37,17 +37,39 @@ export default function CodeOfConduct() {
           sancionados o expulsados de la misma, a discresión de los organizadores.
         </p>
       </section>
-      <section className="my-20">
+      <section className="my-16">
         <h2 className="font-big mb-5 text-xl">La Versión Menos Corta</h2>
-        <p className="font-small text-2xl">
+        <p>
           Los abusos incluyen: comentarios ofensivos relativos a género, orientación sexual, 
           discapacidad, apariencia física, talla, raza, religión, imágenes sexuales en espacios
           públicos, intimidación deliberada, acoso, persecusión, fotografía o grabación de abusos, 
           interrupciones continuadas de charlas u otros eventos, contacto físico inapropiado y 
           atencion sexual no deseada.
+        </p>
+        <p>
           Se espera que los participantes instados a parar cualquier comportamiento abusivo 
           obedezcan inmediatamente.
-          Los patrocinadores también están sujetos a la politica contra abusos.
+        </p>
+        <p>
+          Los patrocinadores también están sujetos a la politica contra abusos. En particular, 
+          los patrocinadores no deberían utilizar imágenes, actividades u otro material con contenido sexual. 
+          El personal (voluntarios incluidos) no debería usar ropa/uniformes/disfraces sexualizados, o que 
+          de otra manera crean un ambiente sexualizado.
+        </p>
+        <p>
+          Si un participante incurre en un comportamiento abusivo, los organizadores del evento pueden tomar 
+          cualquier acción que estimen oportuna, incluyendo advertencias al ofensor o su expulsión del evento.
+        </p>
+        <p>
+          Si estás sufriendo algún abuso, adviertes abusos sobre otra persona, o tienes alguna otra preocupación, 
+          por favor contacta con algún miembro del personal del evento inmediatamente. El personal del evento 
+          puede ser fácilmente identificado, ya que vestirán camisetas del evento.
+        </p>
+        <p>
+          El personal del evento estará atento para ayudar a los participantes a contactar con la seguridad del 
+          lugar del meetup o con las autoridades locales, proveer escolta o asistir de otra manera a aquellos que 
+          estén sufriendo abusos para que se sientan seguros mientras dure el evento. Valoramos tu asistencia.
+          Esperamos que los participantes sigan estas reglas durante el evento y talleres relacionados con el evento.
         </p>
       </section>
     </main>
@@ -55,19 +77,3 @@ export default function CodeOfConduct() {
     </>
   );
 }
-
-    {/* <div className="p-4 shadow rounded bg-white">
-      <h1 className="text-purple-500 leading-normal">Code Of Conduct</h1>
-      <ul className="text-gray-500">
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/previous-events">
-            <a>Previous Events</a>
-          </Link>
-        </li>
-      </ul>
-    </div> */}
