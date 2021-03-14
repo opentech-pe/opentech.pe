c2759d37aa77ddaddf6d3677ee05097a4b0cff38
Paulo Tijero <paulotijero@gmail.com>
2021-03-14 10:42:47 -0500

    Feature/80 add sitemap robots (#84)
    * ADD: next-sitemap
    
    * ADD: script postbuiild to run sitemap generator
    
    * ADD: sitema/robots routes in gitignore
    
    * ADD: config next-sitemap generator

---
+++ next-sitemap.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteUrl: 'https://example.com',
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+}
