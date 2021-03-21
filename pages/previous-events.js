0f7e252875d9c3ea780c5681d6cc7fb0dc69fb7a
paulotijero <paulotijero@gmail.com>
2021-03-21 11:04:05 -0500

    ADD: icons svg in previews events page

--- pages/previous-events.js
+++ pages/previous-events.js
@@ -1,9 +1,7 @@
-import Layout from '../components/layout'
-
-import Events from '../src/data/events.json'
+import Layout from "../components/layout";
+import Events from "../src/data/events.json";
 
 export default function PreviousEvents() {
-
   return (
     <Layout title="Eventos anteriores| OpenTech">
       <main className="px-3 md:px-48">
@@ -13,30 +11,65 @@ export default function PreviousEvents() {
           </h1>
         </section>
         <section>
-          { Events.map((event,i) => (
+          {Events.map((event, i) => (
             <div key={`event#${i}`} className="mb-10">
               <h2 className="font-big mb-5 text-xl">{event.date}</h2>
               <p>El día {event.day} tuvimos las siguientes charlas:</p>
               <div className="ml-5">
                 <ul>
-                  {
-                    event.talks.map((talk, i) => (
-                      <li key={`talk#${i}`}>
-                        <p>#{talk.title} -
-                          <span className="text-blue-500">{talk.speaker}</span>#
-                          <a href={talk.slide} target="_blank">slide</a>#
-                          <a href={talk.speaker_link} target="_blank">social</a>
-                        </p>
-                      </li>)
-                    )
-                  }
+                  {event.talks.map((talk, i) => (
+                    <li key={`talk#${i}`} className="flex flex-col md:flex-row my-5 md:my-1">
+                      <p>
+                        # {talk.title}
+                      </p>
+                      <p className="hidden md:inline-block">&nbsp;-&nbsp;</p>
+                      <p className="text-blue-500">
+                        {talk.speaker}
+                        <a
+                          href={talk.slide}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          type="button"
+                          className="nes-btn is-error my-1 mx-2"
+                        >
+                          <svg className="h-3 w-3">
+                            <use xlinkHref="#slide" />
+                          </svg>
+                        </a>
+                        <a
+                          href={talk.speaker_link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          type="button"
+                          className="nes-btn is-primary my-1 mx-2"
+                        >
+                          <svg className="h-3 w-3">
+                            <use xlinkHref={`#${talk.social_network}`} />
+                          </svg>
+                        </a>
+                      </p>
+                      
+                    </li>
+                  ))}
                 </ul>
               </div>
-              <p>Mira las fotos del evento <a href={event.photos} target="_blank">photos</a></p>
+              <p>
+                Mira las fotos del evento
+                <a
+                  href={event.photos}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  type="button"
+                  className="nes-btn is-primary my-1 mx-2"
+                >
+                  <svg className="h-3 w-3">
+                    <use xlinkHref="#camara" />
+                  </svg>
+                </a>
+              </p>
             </div>
           ))}
         </section>
-
       </main>
     </Layout>
   );
