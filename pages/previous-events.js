1e34c96fefa41435c141faa46f9ebba43da5d9ba
paulotijero <paulotijero@gmail.com>
2021-05-08 23:05:42 -0500

    FIX: change server props by static props

--- pages/previous-events.js
+++ pages/previous-events.js
@@ -3,7 +3,7 @@ import ErrorPage from 'next/error'
 
 import Layout from "../components/layout";
 
-export async function getServerSideProps(ctx) {
+export async function getStaticProps(ctx) {
 
   try {
     const result = await fetch(`${process.env.BASE_PATH}/api/events`)
