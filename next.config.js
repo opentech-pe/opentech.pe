123a12895294fb7cbf93a4d6e5f8f427205f7cbb
Paulo Tijero <paulotijero@gmail.com>
2021-05-08 22:59:26 -0500

    Revert "Hotfix/ssr (#92)"
    This reverts commit 225f924992095e88e64caaad7a8b40fbaa6713be.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 module.exports = {
   env: {
     BASE_PATH: process.env.BASE_PATH,
--- pages/previous-events.js
+++ pages/previous-events.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 import ErrorPage from 'next/error'
 
 import Layout from "../components/layout";
