2a728ad87a28d9f3e739a2b6875b76283f68d69e
Jimmy Loloy <loloy.laurencio@gmail.com>
2023-03-30 16:03:48 -0500

    feat: list previous events by reverse order

--- pages/previous-events.js
+++ pages/previous-events.js
@@ -79,7 +79,7 @@ export default function PreviousEvents() {
                 </a>
               </p>
             </div>
-          ))}
+          )).reverse()}
         </section>
       </main>
     </Layout>
