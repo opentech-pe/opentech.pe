bf653e208e6d829f8375f3aa66bd4fa2b5742376
Jimmy Loloy <loloy.laurencio@gmail.com>
2023-03-30 16:08:37 -0500

    delete: hotjar script

--- pages/_document.js
+++ pages/_document.js
@@ -13,20 +13,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <Script
-            id="hotjar-script"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `(function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:3216561,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-            }}
-          />
         </body>
       </Html>
     );
