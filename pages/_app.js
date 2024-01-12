f65b0a758a49fc39514e212eb60f65fa0bc79d45
Jimmy Loloy <loloy.laurencio@gmail.com>
2024-01-12 12:28:10 -0500

    add: react-ga package for google analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import App from "next/app";
+import Script from "next/script";
 
 import "../public/assets/css/application.scss";
 
@@ -8,6 +9,21 @@ export default class OpenTech extends App {
     const { Component, pageProps } = this.props;
     return (
       <>
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-N48NGM27N8"></script>
+
+        <Script strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+
         <Component {...pageProps} />
       </>
     );
